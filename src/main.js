b632fe1156ce506fc8b097eefeb854ea302470ca
fang <jill83123@gmail.com>
2023-09-10 01:12:42 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import './assets/main.css';
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+
+import PrimeVue from 'primevue/config';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue, { unstyled: true });
+
+app.mount('#app');
