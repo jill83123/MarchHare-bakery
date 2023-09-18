3d43305abc18b722af7d3b09f6f16991f822b289
fang <jill83123@gmail.com>
2023-09-19 01:55:29 +0800

    feat: 完成上傳多張圖片的功能

--- src/components/backend/ProductModal.vue
+++ src/components/backend/ProductModal.vue
@@ -11,10 +11,10 @@
   >
     <div
       data-te-modal-dialog-ref
-      class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px] min-[992px]:max-w-[800px] min-[1200px]:max-w-[1140px]"
+      class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto mt-7 min-[576px]:max-w-[800px] min-[1024px]:max-w-[1140px] px-2 sm:px-5"
     >
       <div
-        class="relative flex flex-col w-full text-current bg-white border-none rounded-md shadow-lg outline-none pointer-events-auto bg-clip-padding dark:bg-neutral-600"
+        class="relative flex flex-col w-full text-current bg-white border-none rounded-md shadow-lg outline-none pointer-events-auto bg-clip-padding dark:bg-neutral-600 max-h-[94vh] lg:max-h-[90vh]"
       >
         <div
           class="flex items-center justify-between flex-shrink-0 p-4 border-b-2 border-opacity-100 rounded-t-md border-neutral-100 dark:border-opacity-50"
@@ -47,17 +47,32 @@
         </div>
 
         <!--Modal body-->
-        <div class="relative p-4">
-          <div class="flex flex-col gap-5 lg:flex-row">
-            <div class="lg:w-5/12">
-              <div class="mb-4">
-                <label for="formFile" class="inline-block mb-1 text-neutral-700">上傳圖片</label>
+        <div class="relative p-4 overflow-y-auto">
+          <div class="flex flex-col-reverse gap-5 lg:flex-row">
+            <div class="border-t-2 lg:w-5/12 lg:border-0">
+              <div class="mt-4 mb-4 lg:mt-0">
+                <label for="formFile" class="flex gap-2 mb-1 text-neutral-700"
+                  >上傳圖片
+                  <div class="flex items-center gap-2 text-gray-500" v-if="loadingIcon">
+                    <div
+                      class="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                      role="status"
+                    >
+                      <span
+                        class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                        >Loading...</span
+                      >
+                    </div>
+                    <p>上傳中</p>
+                  </div>
+                </label>
                 <input
                   class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none"
                   type="file"
                   id="formFile"
-                  ref="photoInput"
-                  @change.prevent="uploadPhoto"
+                  ref="imageFileInput"
+                  @change.prevent="uploadPhoto('file')"
+                  :disabled="loadingIcon"
                 />
               </div>
               <div class="mb-4">
@@ -66,26 +81,67 @@
                   <input
                     type="text"
                     class="relative m-0 block w-[1px] min-w-0 flex-auto rounded-l border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none border-r-0"
-                    placeholder="主要圖片"
+                    placeholder="https://"
                     id="imageUrl"
-                    v-model="tempProduct.imageUrl"
                     aria-label="Recipient's username"
                     aria-describedby="basic-addon2"
+                    ref="imageUrlInput"
+                    :disabled="loadingIcon"
                   />
                   <button
                     type="button"
-                    class="rounded-r border border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 hover:bg-danger hover:text-white hover:border-danger"
+                    class="rounded-r border border-solid border-neutral-300 px-3 py-[0.25rem] text-center text-base font-normal leading-[1.6] text-neutral-700 hover:bg-cerulean hover:text-white hover:border-cerulean"
                     id="basic-addon2"
-                    @click.prevent="clearInput"
-                    >刪除</button
+                    @click.prevent="uploadPhoto('button')"
+                    >新增</button
                   >
                 </div>
               </div>
-              <img
-                :src="tempProduct.imageUrl"
-                :alt="tempProduct.imageUrl"
-                class="object-cover object-center aspect-square"
-              />
+              <div class="relative w-full p-1 aspect-square">
+                <div
+                  class="absolute top-[1%] bottom-[1%] left-[1%] right-[1%] flex items-center justify-center border-2 border-dotted"
+                >
+                  <div class="text-center text-gray-400"
+                    ><h6 class="text-2xl leading-10">主要圖片</h6>
+                    <p>假如目前沒有主要圖片，上傳時會優先補上</p></div
+                  >
+                </div>
+                <div class="relative" v-if="tempProduct.imageUrl">
+                  <img
+                    class="object-cover object-center w-full aspect-square"
+                    :src="tempProduct.imageUrl"
+                    :alt="`${tempProduct.title}-主要圖片`"
+                    :title="`${tempProduct.title}-主要圖片`"
+                  />
+                  <button
+                    class="box-content absolute top-0 right-0 p-2 text-white border-none rounded-none material-symbols-outlined hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                    title="刪除主要圖片"
+                    @click.prevent="delPhoto(tempProduct.imageUrl)"
+                  >
+                    close
+                  </button>
+                </div>
+              </div>
+              <ul class="flex flex-wrap">
+                <li
+                  class="relative w-1/3 p-1"
+                  v-for="(images, index) in tempProduct.imagesUrl"
+                  :key="`${images.title}${index}`"
+                >
+                  <img
+                    class="object-cover object-center w-full aspect-square"
+                    :src="images"
+                    :alt="`${tempProduct.title}-圖片${index + 2}`"
+                  />
+                  <button
+                    class="box-content absolute top-0 right-0 p-2 text-white border-none rounded-none material-symbols-outlined hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                    :title="`刪除圖片${index + 2}`"
+                    @click.prevent="delPhoto(index)"
+                  >
+                    close
+                  </button>
+                </li>
+              </ul>
             </div>
             <div class="lg:w-7/12">
               <div class="mb-4">
@@ -241,25 +297,60 @@ export default {
   data() {
     return {
       tempProduct: {},
+      loadingIcon: false,
     };
   },
   methods: {
-    uploadPhoto() {
-      const imageFile = this.$refs.photoInput.files[0];
-      const formData = new FormData();
-      formData.append('file-to-upload', imageFile);
-
-      const api = `${import.meta.env.VITE_APP_API}/api/${
-        import.meta.env.VITE_APP_PATH
-      }/admin/upload`;
-      this.$http.post(api, formData).then((res) => {
-        if (res.data.success) {
-          this.tempProduct.imageUrl = res.data.imageUrl;
+    uploadPhoto(way) {
+      if (way === 'button' && this.$refs.imageUrlInput.value !== '') {
+        this.loadingIcon = true;
+        if (this.tempProduct.imageUrl === '') {
+          this.tempProduct.imageUrl = this.$refs.imageUrlInput.value;
+          this.loadingIcon = false;
+          return;
         }
-      });
+        if (!this.tempProduct.imagesUrl) {
+          this.tempProduct.imagesUrl = [];
+        }
+        this.tempProduct.imagesUrl.push(this.$refs.imageUrlInput.value);
+        this.$refs.imageUrlInput.value = '';
+        this.loadingIcon = false;
+      }
+
+      if (way === 'file') {
+        this.loadingIcon = true;
+
+        const imageFile = this.$refs.imageFileInput.files[0];
+        const formData = new FormData();
+        formData.append('file-to-upload', imageFile);
+
+        const api = `${import.meta.env.VITE_APP_API}/api/${
+          import.meta.env.VITE_APP_PATH
+        }/admin/upload`;
+        this.$http.post(api, formData).then((res) => {
+          if (res.data.success) {
+            if (this.tempProduct.imageUrl === '') {
+              this.tempProduct.imageUrl = res.data.imageUrl;
+              this.loadingIcon = false;
+            }
+
+            if (!this.tempProduct.imagesUrl) {
+              this.tempProduct.imagesUrl = [];
+            } else {
+              this.tempProduct.imagesUrl.push(res.data.imageUrl);
+            }
+          }
+          this.$refs.imageFileInput.value = '';
+          this.loadingIcon = false;
+        });
+      }
     },
-    clearInput() {
-      this.tempProduct.imageUrl = '';
+    delPhoto(item) {
+      if (this.tempProduct.imageUrl === item) {
+        this.tempProduct.imageUrl = '';
+      } else {
+        this.tempProduct.imagesUrl.splice(item, 1);
+      }
     },
   },
   mixins: [modalMixin],
