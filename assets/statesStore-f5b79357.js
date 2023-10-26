6070816760a3e16c8fd621fac8e5000c6e747276
fang <jill83123@gmail.com>
2023-10-27 01:58:04 +0800

    Updates

---
+++ assets/statesStore-f5b79357.js
@@ -0,0 +1,1 @@
+import{J as a}from"./index-fa6b6820.js";const i=a("states",{state:()=>({messages:[],state:{isLoading:!1,loadingIcon:""}}),actions:{pushToastMessage(e,t,s){if(e)this.messages.push({title:t,message:s,style:"bg-success"});else{const o=typeof s=="string"?[s]:s;this.messages.push({title:t,message:o.join(" 、 "),style:"bg-danger"})}}}});export{i as s};
