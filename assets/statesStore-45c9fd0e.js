1a548f2cd86138f5a29189202664e81420549164
fang <jill83123@gmail.com>
2023-11-04 02:39:17 +0800

    Updates

---
+++ assets/statesStore-45c9fd0e.js
@@ -0,0 +1,1 @@
+import{I as a}from"./index-8e713a6f.js";const i=a("states",{state:()=>({messages:[],state:{isLoading:!1,loadingIcon:""}}),actions:{pushToastMessage(e,t,s){if(e)this.messages.push({title:t,message:s,style:"bg-success"});else{const o=typeof s=="string"?[s]:s;this.messages.push({title:t,message:o.join(" 、 "),style:"bg-danger"})}}}});export{i as s};
