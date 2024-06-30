408561b22515809ece9b7e5130b74e8951c06d6a
YU-FANG <jill83123@gmail.com>
2024-06-30 12:44:29 +0800

    chore (vite.config.js): 調整 base 路徑

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MarchHare-bakery/',
+  base: process.env.NODE_ENV === 'production' ? '/MarchHare-bakery/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
